c5cb7800ce1461426e2983e30f7cd58331754fe1
Mikita <53656051+MikiZdanovich@users.noreply.github.com>
2023-08-17 11:37:42 +0200

    Update test_google_search.py
    remove comments

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -7,22 +7,17 @@ from src.pages.google_search_page import GoogleSearchPage
 def test_google_search(driver):
     page = GoogleSearchPage(driver)
 
-    # Navigate to Google
     page.navigate_to()
 
-    # Search for Python
     page.search("Python")
 
-    # Get and print search results links
     links = page.get_search_links()
     for link in links:
         print(link)
 
-    # Assertions
     assert "Python" in page.get_first_result_text()
     assert page.get_num_results() > 1000
 
-    # Store report as json file
     report = {
         "links": links,
         "first_search_result_text": page.get_first_result_text(),
