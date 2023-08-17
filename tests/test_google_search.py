01cd54b93791e555b56f58e2deffe3d3f9523191
Mikita <53656051+MikiZdanovich@users.noreply.github.com>
2023-08-17 11:39:49 +0200

    Update test_google_search.py

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -1,29 +1,27 @@
 import pytest
 import json
-from src.pages.google_search_page import GoogleSearchPage
 
 
 @pytest.mark.smoke
-def test_google_search(driver):
-    page = GoogleSearchPage(driver)
+def test_google_search(google_search_page):
 
-    page.navigate_to()
+    google_search_page.navigate_to()
 
-    page.search("Python")
+    google_search_page.search("Python")
 
-    links = page.get_search_links()
+    links = google_search_page.get_search_links()
     for link in links:
         print(link)
 
-    assert "Python" in page.get_first_result_text()
-    assert page.get_num_results() > 1000
+    assert "Python" in google_search_page.get_first_result_text()
+    assert google_search_page.get_num_results() > 1000
 
     report = {
         "links": links,
-        "first_search_result_text": page.get_first_result_text(),
-        "first_search_result_verification": "Python" in page.get_first_result_text(),
-        "number_of_search_results": page.get_num_results(),
-        "number_of_search_results_verification": page.get_num_results() > 1000,
+        "first_search_result_text": google_search_page.get_first_result_text(),
+        "first_search_result_verification": "Python" in google_search_page.get_first_result_text(),
+        "number_of_search_results": google_search_page.get_num_results(),
+        "number_of_search_results_verification": google_search_page.get_num_results() > 1000,
     }
 
     with open("report.json", "w") as f:
